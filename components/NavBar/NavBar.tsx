08b611acec7d8b5edfc6827ff2ebfcff1f2200bc
mrhaffner <mrhaffner13@gmail.com>
2021-10-10 16:51:38 -0500

    feat/style: added SearchBar component, Normalize.css, button css reset, and Poppins font

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,6 +1,10 @@
 import DesktopMenuLink from './DesktopMenuLink';
 import Logo from './Logo';
+import SearchBar from './SearchBar';
 import styles from './styles.module.scss';
+
+//eventually needs state to control what links are active
+//there will never be a working Magazine
 
 const NavBar = () => {
   return (
@@ -11,6 +15,9 @@ const NavBar = () => {
           <DesktopMenuLink text="snow reports" url="/" active={false} />
           <DesktopMenuLink text="trip planning" url="/" active={true} />
           <DesktopMenuLink text="magazine" url="/" active={false} />
+        </div>
+        <div className={styles.container}>
+          <SearchBar />
         </div>
       </div>
     </nav>
